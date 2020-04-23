7e0353db452b1d84cc942a4dcbc0b133c507dac2
tjdudwns@gmail.com <seoyj2824!!>
2020-04-23 16:54:20 +0900

    first

--- src/main/java/local/PurchasingSystemController.java
+++ src/main/java/local/PurchasingSystemController.java
@@ -14,37 +14,55 @@ import java.util.List;
      @Autowired
      PurchasingSystemRepository purchasingSystemRepository;
 
-@RequestMapping(value = "/purchase",
-        method = RequestMethod.POST,
-        produces = "application/json;charset=UTF-8")
+     @RequestMapping(value = "/purchase",
+             method = RequestMethod.POST,
+             produces = "application/json;charset=UTF-8")
 
-public void purchase(@RequestBody Purchased purchased)
- {
-        System.out.println("##### /purchasingSystem/purchase  called #####");
+     public void purchase(@RequestBody Purchased purchased) {
+         System.out.println("##### /purchasingSystem/purchase  called #####");
 
-        PurchasingSystem purchasingSystem = new PurchasingSystem();
-        purchasingSystem.setCustomerId(purchased.getCustomerId());
-        purchasingSystemRepository.save(purchasingSystem);
+         PurchasingSystem purchasingSystem = new PurchasingSystem();
+         purchasingSystem.setCustomerId(purchased.getCustomerId());
+         purchasingSystem.setId(purchased.getId());
+         purchasingSystem.setMovieId(purchased.getMovieId());
+         purchasingSystem.setMovieName(purchased.getMovieName());
+         purchasingSystem.setStartNo(0);
+         purchasingSystemRepository.save(purchasingSystem);
 
 
+     }
 
-        }
+     @RequestMapping(value = "/cancel",
+             method = RequestMethod.POST,
+             produces = "application/json;charset=UTF-8")
 
-@RequestMapping(value = "/cancel",
-        method = RequestMethod.POST,
-        produces = "application/json;charset=UTF-8")
+     public void purchaseCancellation(@RequestBody Canceled canceled) {
+         System.out.println("##### /purchasingSystem/canceled  called #####");
 
-public void purchaseCancellation(HttpServletRequest request, HttpServletResponse response)
-        throws Exception {
-        System.out.println("##### /purchasingSystem/purchaseCancellation  called #####");
-        }
+         PurchasingSystem purchasingSystem = new PurchasingSystem();
+         purchasingSystem.setCustomerId(canceled.getCustomerId());
+         purchasingSystem.setId(canceled.getId());
+         purchasingSystem.setMovieId(canceled.getMovieId());
+         purchasingSystem.setMovieName(canceled.getMovieName());
+         purchasingSystem.setStartNo(0);
+         purchasingSystemRepository.save(purchasingSystem);
 
-@RequestMapping(value = "/evaluate",
-        method = RequestMethod.POST,
-        produces = "application/json;charset=UTF-8")
 
-public void evaluation(HttpServletRequest request, HttpServletResponse response)
-        throws Exception {
-        System.out.println("##### /purchasingSystem/evaluation  called #####");
-        }
+     }
+
+     @RequestMapping(value = "/evaluate",
+             method = RequestMethod.POST,
+             produces = "application/json;charset=UTF-8")
+
+     public void evaluation(@RequestBody Rated rated) {
+         System.out.println("##### /purchasingSystem/evaluate  called #####");
+
+         PurchasingSystem purchasingSystem = new PurchasingSystem();
+         purchasingSystem.setCustomerId(rated.getCustomerId());
+         purchasingSystem.setId(rated.getId());
+         purchasingSystem.setMovieId(rated.getMovieId());
+         purchasingSystem.setMovieName(rated.getMovieName());
+         purchasingSystem.setStartNo(rated.getStartNo());
+         purchasingSystemRepository.save(purchasingSystem);
+     }
  }
