025e0d6dcde8fb2d26d79947356cd18f853fb177
tjdudwns@gmail.com <seoyj2824!!>
2020-04-23 10:53:11 +0900

    first

--- src/main/java/local/PurchasingSystemController.java
+++ src/main/java/local/PurchasingSystemController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
  @RestController
+ @RequestMapping(value = "api/purchasing")
  public class PurchasingSystemController {
 
      @Autowired
